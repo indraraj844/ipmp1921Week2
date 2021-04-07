72680e96227064b8e524d68d92cfa3c8c99fc15e
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-04-07 23:00:42 +0530

    Add files via upload

---
+++ Week2i/hackrank practice/minimum number of swap.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+void swap_nodes(vector<int>&arr,int i,int j)
+{
+    int t=arr[i];
+    arr[i]=arr[j];
+    arr[j]=t;
+}
+
+// Complete the minimumSwaps function below.
+int minimumSwaps(vector<int> arr) {
+    unordered_map<int,int>hash;
+    int n=arr.size();
+    int temp[n];
+    int count=0;
+for(int i=0;i<n;i++)
+{
+    temp[i]=arr[i];    
+}
+sort(temp,temp+n);
+for(int i=0;i<n;i++)
+{
+    hash[arr[i]]=i; 
+}
+for(int i=0;i<n;i++)
+{
+    if(arr[i]!=temp[i])
+    {
+        count++;
+        int t=arr[i];
+        swap_nodes(arr,i,hash[temp[i]]);
+        int t1=hash[t];
+        hash[arr[i]]=i;
+        hash[t]=t;
+        
+    }    
+}
+return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = minimumSwaps(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
