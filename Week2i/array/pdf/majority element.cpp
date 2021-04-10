b0276f6d139c86e19208d7e5fbd182e8544c99a6
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-04-10 15:00:31 +0530

    Update majority element.cpp

--- Week2i/array/pdf/majority element.cpp
+++ Week2i/array/pdf/majority element.cpp
@@ -29,3 +29,51 @@ int main()
 
 method 2:
 find candidate method
+#include<bits/stdc++.h>
+using namespace std;
+
+int find_candidate(int arr[],int n)
+{
+    int count=1;
+    int maj_index=0;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[maj_index]==arr[i])
+        count++;
+        else
+        {
+            count--; 
+        }
+       
+        if(!count)
+        {
+            maj_index=i;
+            count=1;
+        }
+    }
+    return maj_index;
+}
+bool isCheck(int arr[],int l,int n)
+{
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==arr[l])
+        count++;
+    }
+    return ((n+1)/2)<=count;
+
+}
+int main()
+{
+    int arr[7]={2,3,2,2,3,2,8};
+    int n=7;
+    int l=find_candidate(arr,n);
+    if(isCheck(arr,l,n))
+    cout<<"majority element is "<<arr[l];
+    else
+    cout<<"no majority element";
+    return 0;
+}
+method 3.
+use hashmap
