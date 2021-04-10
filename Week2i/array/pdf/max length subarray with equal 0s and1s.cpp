59fbb0cb9df1c25a50d3351ddbed918392a59775
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-04-10 17:14:16 +0530

    Update max length subarray with equal 0s and1s.cpp

--- Week2i/array/pdf/max length subarray with equal 0s and1s.cpp
+++ Week2i/array/pdf/max length subarray with equal 0s and1s.cpp
@@ -34,3 +34,47 @@ int main()
 }
 method 2:
 efficent
+using hashmap 
+#include<bits/stdc++.h>
+using namespace std;
+
+int max_subarray(int arr[],int n)
+{
+    int sum=0;
+    int max_length=0,l;
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        
+        if (sum == 0) {
+            max_length= i + 1;
+        }
+        if(mp.find(sum)==mp.end())
+            mp[sum]=i;
+        else
+        {
+         l=distance(mp.find(sum),mp.end());
+        }
+         max_length=max(max_length,l+1);
+
+    }
+    return max_length;
+
+}
+
+int main()
+{
+    int arr[7]={1,0,0,1,1,0,1};
+    int n=7;
+    for(int i=0;i<n;i++)
+    {
+        if(!arr[i])
+        arr[i]=-1;
+    }
+    cout<<" maximum length of equal zero and equal 1s="
+    <<max_subarray(arr,n);
+    return 0;
+    
+}
+thankyou
